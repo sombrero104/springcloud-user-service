76a1ff6ac6a6778137c8bb36665fdb2bfe85ef34
sombrero104 <sombrero.khy@gmail.com>
2022-06-25 23:20:10 +0900

    .

--- src/main/java/me/UsersController.java
+++ src/main/java/me/UsersController.java
@@ -1,5 +1,7 @@
 package me;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class UsersController {
 
+    private Environment environment;
+
+    @Autowired
+    public UsersController(Environment environment) {
+        this.environment = environment;
+    }
+
     @GetMapping("/health_check")
     public String status() {
         return "It's working in user service.";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return environment.getProperty("greeting.message");
     }
 
 }
