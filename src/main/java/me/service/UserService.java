083f085ad56115e0a9d3146621798ff24c2c53ea
sombrero104 <sombrero.khy@gmail.com>
2022-07-07 22:43:05 +0900

    .

--- src/main/java/me/service/UserService.java
+++ src/main/java/me/service/UserService.java
@@ -2,13 +2,19 @@ package me.service;
 
 import me.dto.UserDto;
 import me.jpa.UserEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
+
+    UserDetails loadUserByUsername(String username);
 
     UserDto createUser(UserDto userDto);
 
     UserDto getUserByUserId(String userId);
 
     Iterable<UserEntity> getUserByAll();
+
+    UserDto getUserDetailsByEmail(String email);
 
 }
